bc59ba332cd226d39c4bc2c5a88cd756df9fef28
jugu <jugu.87@gmail.com>
2016-03-02 12:48:14 -0500

    website

---
+++ scripts/jugu.js
@@ -0,0 +1,28 @@
+latlong = {"latitude":50,"longitude":50}
+function getLocation() {
+    if (navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {         
+    }
+}
+
+function showPosition(position)
+{
+	latlong.latitude = position.coords.latitude;
+	latlong.longitude = position.coords.longitude;
+}
+
+function getWeather()
+{	
+	/*key = "0ec0275ea15fd6c1017256c4be7e02a4";
+	url = "http://api.openweathermap.org/data/2.5/weather?lat="+latlong.latitude+"&lon="+latlong.longitude+"&APPID="+key;
+	$.get(
+		url,
+    {},
+    function(data) {
+		
+        alert(data);  // "google.com"
+    }
+);*/
+	
+}
